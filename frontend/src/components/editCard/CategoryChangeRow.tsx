d91b34e5d1764155974074f3c719860c65a0cb56
Infinite <infinitekittens@protonmail.com>
2020-09-04 20:33:01 +0000

    Tweak

--- frontend/src/components/editCard/CategoryChangeRow.tsx
+++ frontend/src/components/editCard/CategoryChangeRow.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@apollo/client";
 import { loader } from "graphql.macro";
 
 import { Category, CategoryVariables } from "src/definitions/Category";
+import ChangeRow from "./ChangeRow";
 
 const CategoryQuery = loader("src/queries/Category.gql");
 
@@ -25,13 +26,12 @@ const CategoryChangeRow: React.FC<CategoryChangeRowProps> = ({
   });
 
   return (
-    <div className="row">
-      <b className="col-2 text-right">Category</b>
-      {showDiff && <span className="col-2 bg-danger">{oldCategory ?? ""}</span>}
-      <span className={`col-2 ${showDiff && "bg-success"}`}>
-        {data?.findTagCategory?.name}
-      </span>
-    </div>
+    <ChangeRow
+      name="Category"
+      oldValue={oldCategory}
+      newValue={data?.findTagCategory?.name}
+      showDiff={showDiff}
+    />
   );
 };
 
