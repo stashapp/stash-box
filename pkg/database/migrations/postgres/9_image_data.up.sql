b16258b17dbc548cbbfb7310b3f08ea591f80529
Infinite <infinitekittens@protonmail.com>
2021-01-11 22:51:38 +0000

    Make checksum/width/height columns not null.

--- pkg/database/migrations/postgres/9_image_data.up.sql
+++ pkg/database/migrations/postgres/9_image_data.up.sql
@@ -1,4 +1,6 @@
-ALTER TABLE "images" ADD COLUMN "checksum" varchar(255);
+ALTER TABLE "images" ADD COLUMN "checksum" varchar(255) NOT NULL;
 ALTER TABLE "images" ALTER COLUMN "url" DROP NOT NULL;
+ALTER TABLE "images" ALTER COLUMN "width" SET NOT NULL;
+ALTER TABLE "images" ALTER COLUMN "height" SET NOT NULL;
 
 CREATE UNIQUE INDEX "images_checksum_idx" ON "images" ("checksum");
