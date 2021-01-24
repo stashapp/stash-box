3d897fa2b0cd8502cb5ee611a5bb6859ad68b48e
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2021-01-24 10:03:09 +0100

    Push prod/dev images to dockerhub (#40)

---
+++ docker/ci/x86_64/docker_push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_TAG=$1
+
+# must build the image from dist directory
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t stashapp/stash-box:$DOCKER_TAG -f ./docker/ci/x86_64/Dockerfile ./dist
+
+docker push stashapp/stash-box:$DOCKER_TAG
