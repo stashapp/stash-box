556ce97b7f6b74edfc56464daba981ba2328610f
Infinite <infinitekittens@protonmail.com>
2021-05-03 21:09:03 +0000

    Prevent submission of fingerprints with 0s duration

--- pkg/models/model_scene.go
+++ pkg/models/model_scene.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/gofrs/uuid"
 
@@ -140,12 +141,14 @@ func CreateSceneFingerprints(sceneID uuid.UUID, fingerprints []*FingerprintInput
 	var ret SceneFingerprints
 
 	for _, fingerprint := range fingerprints {
-		ret = append(ret, &SceneFingerprint{
-			SceneID:   sceneID,
-			Hash:      fingerprint.Hash,
-			Algorithm: fingerprint.Algorithm.String(),
-			Duration:  fingerprint.Duration,
-		})
+		if fingerprint.Duration > 0 {
+			ret = append(ret, &SceneFingerprint{
+				SceneID:   sceneID,
+				Hash:      fingerprint.Hash,
+				Algorithm: fingerprint.Algorithm.String(),
+				Duration:  fingerprint.Duration,
+			})
+		}
 	}
 
 	return ret
