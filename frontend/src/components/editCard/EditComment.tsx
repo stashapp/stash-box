480f1a34e5267518575fbba3503bc527c89d9af1
Infinite <infinitekittens@protonmail.com>
2021-05-07 21:10:57 +0000

    Add markdown to edit comments

--- frontend/src/components/editCard/EditComment.tsx
+++ frontend/src/components/editCard/EditComment.tsx
@@ -1,23 +1,46 @@
 import React from "react";
+import Marked, { MarkedOptions } from "marked";
+import DOMPurify from "dompurify";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-import { Edit_findEdit_comments as Comment } from "src/graphql/definitions/Edit";
+import { Edit_findEdit_comments_user as User } from "src/graphql/definitions/Edit";
 import { formatDateTime, userHref } from "src/utils";
 
 const CLASSNAME = "EditComment";
 
 interface Props {
-  comment: Comment;
+  comment: string;
+  date: string;
+  user?: Pick<User, "name">;
 }
 
-const EditComment: React.FC<Props> = ({ comment }) => (
+DOMPurify.addHook("afterSanitizeAttributes", (node: Element) => {
+  if (node.tagName === "A") {
+    node.setAttribute("target", "_blank");
+    node.setAttribute("rel", "noopener nofollow");
+  }
+});
+
+const options: MarkedOptions = {
+  gfm: true,
+  breaks: true,
+};
+
+const EditComment: React.FC<Props> = ({ comment, date, user }) => (
   <Card className={CLASSNAME}>
-    <Card.Body className="pb-0">{comment.comment}</Card.Body>
+    <Card.Body className="pb-0">
+      {/* eslint-disable-next-line react/no-danger */}
+      <div
+        dangerouslySetInnerHTML={{
+          __html: DOMPurify.sanitize(Marked(comment, options)),
+        }}
+      />
+    </Card.Body>
     <Card.Footer className="text-right">
-      <Link to={userHref(comment.user)}>{comment.user.name}</Link>
+      {user && <Link to={userHref(user)}>{user.name}</Link>}
       <span className="mx-1">&bull;</span>
-      <span>{formatDateTime(comment.date, false)}</span>
+      <span>{formatDateTime(date, false)}</span>
     </Card.Footer>
   </Card>
 );
