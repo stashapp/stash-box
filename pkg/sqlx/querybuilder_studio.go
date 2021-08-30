7387f2bc07f59ac816a2d92152958c99edb53f8a
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-08-30 23:51:34 +0000

    Exclude delete studios from query

--- pkg/sqlx/querybuilder_studio.go
+++ pkg/sqlx/querybuilder_studio.go
@@ -127,6 +127,8 @@ func (qb *studioQueryBuilder) Query(studioFilter *models.StudioFilterType, findF
 	query := newQueryBuilder(studioDBTable)
 	query.Body += "LEFT JOIN studios as parent_studio ON studios.parent_studio_id = parent_studio.id"
 
+	query.Eq("studios.deleted", false)
+
 	if q := studioFilter.Name; q != nil && *q != "" {
 		searchColumns := []string{"studios.name"}
 		clause, thisArgs := getSearchBinding(searchColumns, *q, false, true)
