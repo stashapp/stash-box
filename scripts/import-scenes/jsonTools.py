fa5b16392504dfa4e1d69cf075ef774a921ca6e8
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2021-09-18 04:06:53 +0000

    Refactor, add wget

--- scripts/import-scenes/jsonTools.py
+++ scripts/import-scenes/jsonTools.py
@@ -3,46 +3,163 @@ import json
 import sys
 import datetime
 import re
+import requests
+import hashlib
+import os
 
-file = None
-out = None
-buffer = []
+class State:
+    file = None
+    out = None
+    buffer = []
+    data = None
 
-def write(data, fn):
-    global out
-    if out == None:
-        out = open(fn, 'w')
+    outTags = None
+    tagsBuffer = {}
+    outPerformers = None
+    performersBuffer = {}
+    outStudios = None
+    studiosBuffer = {}
+
+    def __init__(self) -> None:
+        self.args = sys.argv[1:]
+
+    def usage(self, ln, usage):
+        if len(self.args) < ln:
+            raise Exception(usage)
+
+    def arg(self):
+        return self.args.pop(0)
+
+    def finalise(self):
+        if self.out != None:
+            out = open(self.out, 'w')
+            json.dump(self.buffer, out, indent=1)
+            out.close()
+
+class CommandSpec:
+    usage = ""
+    func = None
+    nargs = 0
+    branched = False
+
+    def __init__(self, func, nargs, usage, branched = False) -> None:
+        self.usage = usage
+        self.func = func
+        self.nargs = nargs
+        self.branched = branched
+
+    def parseArgs(self, args):
+        if len(args) < self.nargs:
+            raise Exception(self.usage)
+
+        ret = []
+        for _ in range(self.nargs):
+            ret.append(args.pop(0))
+
+        return ret
+
+
+class Command:
+    func = None
+    args = []
+    commands = []
+
+    def __init__(self, args, spec: CommandSpec) -> None:
+        self.func = spec.func
+        self.args = spec.parseArgs(args)
+
+        if spec.branched:
+            self.commands = makeCommands(args)
     
-    buffer.append(data)
+    def execute(self, state: State):
+        self.func(state, self)
 
-def rm(data, field):
-    if field in data:
-        data.pop(field)
+    def executeSubCommands(self, state: State):
+        for c in self.commands:
+            c.execute(state)
 
-def keep(data, fields):
+def makeCommands(args):
+    ret = []
+    while len(args) > 0:
+        cmd = args.pop(0).lower()
+
+        if cmd not in commands:
+            raise Exception("unknown command", cmd)
+
+        spec = commands[cmd]
+        ret.append(Command(args, spec))
+
+    return ret
+
+def readCSV(state: State, cmd: Command):
+    fn = cmd.args[0]
+
+    with open(fn, newline='') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            state.data = row
+            cmd.executeSubCommands(state)
+
+def readJSON(state: State, cmd: Command):
+    fn = cmd.args[0]
+
+    with open(fn) as file:
+        reader = json.load(file)
+
+        for row in reader:
+            state.data = row
+            cmd.executeSubCommands(state)
+
+def write(state: State, cmd: Command):
+    fn = cmd.args[0]
+    
+    if state.out == None:
+        state.out = fn
+    
+    state.buffer.append(state.data)
+
+def rm(state: State, cmd: Command):
+    field = cmd.args[0]
+
+    if field in state.data:
+        state.data.pop(field)
+
+def keep(state: State, cmd: Command):
+    fields = cmd.args[0].split(",")
+
     toRemove = []
-    for df in data:
+    for df in state.data:
         if df not in fields:
             toRemove.append(df)
     
     for f in toRemove:
-        rm(data, f)
+        rm(state.data, f)
 
-def mv(data, frm, to):
-    if frm in data:
-        data[to] = data[frm]
-        data.pop(frm)
+def mv(state: State, cmd: Command):
+    frm = cmd.args[0]
+    to = cmd.arg[1]
 
-def parseDate(data, field, format):
-    dt = datetime.datetime.strptime(data[field], format)
-    data[field] = dt.strftime("%Y-%m-%d")
+    if frm in state.data:
+        state.data[to] = state.data[frm]
+        state.data.pop(frm)
 
-def parseDuration(data, field, format):
+def parseDate(state: State, cmd: Command):
+    field = cmd.args[0]
+    format = cmd.args[1]
+
+    dt = datetime.datetime.strptime(state.data[field], format)
+    state.data[field] = dt.strftime("%Y-%m-%d")
+
+def parseDuration(state: State, cmd: Command):
+    field = cmd.args[0]
+    format = cmd.args[1]
+
     # example
     # "(?:(?:(?P<hours>\d+):)?(?:(?P<minutes>\d+):))?(?P<seconds>\d+)"
     regex = re.compile(format)
 
-    parts = regex.match(data[field])
+    parts = regex.match(state.data[field])
     if not parts:
         return
     
@@ -54,72 +171,65 @@ def parseDuration(data, field, format):
     td = datetime.timedelta(**time_params)
     
     # convert to seconds
-    data[field] = int(td.total_seconds())
+    state.data[field] = int(td.total_seconds())
 
-def replace(data, field, regex, rpl):
+def replace(state: State, cmd: Command):
+    field = cmd.args[0]
+    regex = cmd.args[1]
+    repl = cmd.args[2]
+
     regex = re.compile(regex)
-    data[field] = regex.sub(rpl, data[field])
+    state.data[field] = regex.sub(repl, state.data[field])
 
-def execute(data, args):
-    if len(args) > 0:
-        cmd = args.pop(0).lower()
+def split(state: State, cmd: Command):
+    field = cmd.args[0]
+    sep = cmd.args[1]
 
-        if cmd == "write":
-            fn = args.pop(0)
-            write(data, fn)
-        elif cmd == "rm":
-            field = args.pop(0)
-            rm(data, field)
-        elif cmd == "mv":
-            field = args.pop(0)
-            to = args.pop(0)
-            mv(data, field, to)
-        elif cmd == "keep":
-            fields = args.pop(0).split(",")
-            keep(data, fields)
-        elif cmd == "replace":
-            field = args.pop(0)
-            regex = args.pop(0)
-            repl = args.pop(0)
-            replace(data, field, regex, repl)
-        elif cmd == "parsedate":
-            field = args.pop(0)
-            format = args.pop(0)
-            parseDate(data, field, format)
-        elif cmd == "parseduration":
-            field = args.pop(0)
-            format = args.pop(0)
-            parseDuration(data, field, format)
-        else:
-            raise Exception("unknown command", cmd)
+    state.data[field] = state.data[field].split(sep)
 
-def executeCommands(data):
-    localCmds = sys.argv[1:]
+def wget(state: State, cmd: Command):
+    field = cmd.args[0]
+    outDir = cmd.args[1]
 
-    while len(localCmds) > 0:
-        execute(data, localCmds)
+    url = state.data[field]
+    try:
+        r = requests.get(url)
+    except:
+        print("Failed to get url {}, skipping".format(url))
+        return
+    
+    filename = os.path.join(outDir, hashlib.md5(url.encode("utf-8")).hexdigest())
+
+    if not os.path.exists(filename):
+        print("Downloading {}".format(url))
+        with open(filename, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+
+    state.data[field] = filename
+
+commands = {
+    "readcsv": CommandSpec(readCSV, 1, "readcsv <filename>", True),
+    "readjson": CommandSpec(readJSON, 1, "readjson <filename>", True),
+    "write": CommandSpec(write, 1, "write <filename>"),
+    "rm": CommandSpec(rm, 1, "rm <field>"),
+    "mv": CommandSpec(mv, 2, "mv <field> <to>"),
+    "keep": CommandSpec(keep, 1, 'keep <"field,...">'),
+    "replace": CommandSpec(replace, 3, "replace <field> <regex> <replace with>"),
+    "parsedate": CommandSpec(parseDate, 2, "parsedate <field> <format>"),
+    "parseduration": CommandSpec(parseDuration, 2, "parseduration <field> <format>"),
+    "split": CommandSpec(split, 2, "split <field> <separator>"),
+    "wget": CommandSpec(wget, 2, "wget <field> <dir>")
+}
 
 def main():
-    cmd = sys.argv[1]
-    fn = sys.argv[2]
-    if cmd.lower() == "readcsv":
-        with open(fn, newline='') as file:
-            reader = csv.DictReader(file)
+    commands = makeCommands(sys.argv[1:])
 
-            for row in reader:
-                execute(row, 3)
-    elif cmd.lower() == "readjson":
-        with open(fn) as file:
-            reader = json.load(file)
+    state = State()
 
-            for row in reader:
-                execute(row, 3)
-
-    if file != None:
-        file.close()
-
-    if out != None:
-        json.dump(buffer, out, indent=1)
-        out.close()
+    for c in commands:
+        c.execute(state)
+    
+    state.finalise()
 
 main()
