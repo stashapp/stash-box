de507c86ed5f94322254d22d8f801d05f0657b76
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2021-10-26 08:52:33 +0200

    Cache user locally to optimize frontend bootstrapping (#143)

--- frontend/src/graphql/definitions/AddUser.ts
+++ frontend/src/graphql/definitions/AddUser.ts
@@ -20,7 +20,7 @@ export interface AddUser_userCreate {
   /**
    * Should not be visible to other users
    */
-  roles: RoleEnum[] | null;
+  roles: RoleEnum[];
 }
 
 export interface AddUser {
--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as usePagination } from "./UsePagination";
 export { default as useEditFilter } from "./UseEditFilter";
+export { default as useAuth } from "./useAuth";
