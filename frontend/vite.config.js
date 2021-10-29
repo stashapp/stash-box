54656abd57bf690218dd1fe4d1ea86dfc81499fa
Infinite <infinitekittens@protonmail.com>
2021-10-29 11:27:24 +0000

    Tweak config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import graphqlPlugin from "@rollup/plugin-graphql";
+import analyzePlugin from "rollup-plugin-analyzer";
 
-export default defineConfig({
+const config = {
   build: {
     outDir: 'build',
   },
   optimizeDeps: {
     entries: "src/index.tsx",
   },
-  plugins: [react(), tsconfigPaths(), graphqlPlugin()],
-}) 
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    graphqlPlugin(),
+  ],
+};
+
+if (process.env.analyze) {
+  config.plugins.push(
+    analyzePlugin({ summaryOnly: true, limit: 30 })
+  );
+}
+
+export default defineConfig(config);
