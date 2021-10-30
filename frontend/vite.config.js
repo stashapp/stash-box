5ba643f0169ad4ca0657dbdbaeb8d5dc5450335c
peolic <66393006+peolic@users.noreply.github.com>
2021-10-30 10:19:36 +0300

    Fix vite environment variables (#149)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import graphqlPlugin from "@rollup/plugin-graphql";
 import analyzePlugin from "rollup-plugin-analyzer";
 
-const config = {
-  build: {
-    outDir: 'build',
-  },
-  optimizeDeps: {
-    entries: "src/index.tsx",
-  },
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    graphqlPlugin(),
-  ],
-};
+export default defineConfig(({ mode }) => {
+  const env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd(), ""),
+  };
 
-if (process.env.analyze) {
-  config.plugins.push(
-    analyzePlugin({ summaryOnly: true, limit: 30 })
-  );
-}
+  /** @type {import("vite").UserConfig} */
+  const config = {
+    build: {
+      outDir: "build",
+    },
+    optimizeDeps: {
+      entries: "src/index.tsx",
+    },
+    server: {
+      port: Number(env.PORT) || undefined,
+    },
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      graphqlPlugin(),
+    ],
+  };
 
-export default defineConfig(config);
+  if (process.env.analyze) {
+    config.plugins.push(
+      analyzePlugin({ summaryOnly: true, limit: 30 })
+    );
+  }
+
+  return config;
+});
