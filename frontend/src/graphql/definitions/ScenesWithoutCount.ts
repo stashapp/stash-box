7488469ec1c715f7ea1d97cc1a6b02e44edb755e
InfiniteStash <infinitestash@protonmail.com>
2021-11-23 21:44:11 +0100

    Add scene queries without count for landing page (#180)

---
+++ frontend/src/graphql/definitions/ScenesWithoutCount.ts
@@ -0,0 +1,77 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+
+import { QuerySpec, SceneFilterType, GenderEnum } from "./globalTypes";
+
+// ====================================================
+// GraphQL query operation: ScenesWithoutCount
+// ====================================================
+
+
+export interface ScenesWithoutCount_queryScenes_scenes_urls {
+  __typename: "URL";
+  url: string;
+  type: string;
+}
+
+export interface ScenesWithoutCount_queryScenes_scenes_images {
+  __typename: "Image";
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface ScenesWithoutCount_queryScenes_scenes_studio {
+  __typename: "Studio";
+  id: string;
+  name: string;
+}
+
+export interface ScenesWithoutCount_queryScenes_scenes_performers_performer {
+  __typename: "Performer";
+  id: string;
+  name: string;
+  disambiguation: string | null;
+  deleted: boolean;
+  gender: GenderEnum | null;
+  aliases: string[];
+}
+
+export interface ScenesWithoutCount_queryScenes_scenes_performers {
+  __typename: "PerformerAppearance";
+  /**
+   * Performing as alias
+   */
+  as: string | null;
+  performer: ScenesWithoutCount_queryScenes_scenes_performers_performer;
+}
+
+export interface ScenesWithoutCount_queryScenes_scenes {
+  __typename: "Scene";
+  id: string;
+  date: any | null;
+  title: string | null;
+  duration: number | null;
+  urls: ScenesWithoutCount_queryScenes_scenes_urls[];
+  images: ScenesWithoutCount_queryScenes_scenes_images[];
+  studio: ScenesWithoutCount_queryScenes_scenes_studio | null;
+  performers: ScenesWithoutCount_queryScenes_scenes_performers[];
+}
+
+export interface ScenesWithoutCount_queryScenes {
+  __typename: "QueryScenesResultType";
+  scenes: ScenesWithoutCount_queryScenes_scenes[];
+}
+
+export interface ScenesWithoutCount {
+  queryScenes: ScenesWithoutCount_queryScenes;
+}
+
+export interface ScenesWithoutCountVariables {
+  filter?: QuerySpec | null;
+  sceneFilter?: SceneFilterType | null;
+}
