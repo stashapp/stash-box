0a6ea9f34a8363e404dc71e56a71d9ad62f6e753
Infinite <infinitekittens@protonmail.com>
2021-12-18 11:34:59 +0000

    Show edit vote status to edit owner

--- frontend/src/components/editCard/VoteBar.tsx
+++ frontend/src/components/editCard/VoteBar.tsx
@@ -26,12 +26,22 @@ const VoteBar: FC<Props> = ({ edit }) => {
   const [vote, setVote] = useState<VoteTypeEnum | null>(userVote?.vote ?? null);
   const [submitVote, { loading: savingVote }] = useVote();
 
-  if (
-    edit.status !== VoteStatusEnum.PENDING ||
-    !canVote(auth.user) ||
-    auth.user?.id === edit.user?.id
-  )
+  if (edit.status !== VoteStatusEnum.PENDING || !canVote(auth.user))
     return <></>;
+
+  if (auth.user?.id === edit.user?.id)
+    return (
+      <div className={CLASSNAME}>
+        <div className={CLASSNAME_SAVE}>
+          <h6>
+            <span className="me-2">Current Vote:</span>
+            <span>{`${edit.vote_count > 0 ? "+" : ""}${
+              edit.vote_count === 0 ? "-" : edit.vote_count
+            }`}</span>
+          </h6>
+        </div>
+      </div>
+    );
 
   const handleSave = () => {
     if (!vote) return;
