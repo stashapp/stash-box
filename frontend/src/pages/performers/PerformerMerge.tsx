3660238d53b1b5fd0c0d6eb412e264fc8a35f8d0
peolic <66393006+peolic@users.noreply.github.com>
2022-02-06 20:51:54 +0200

    Fix "0" when scene duration is 0 (#286)

--- frontend/src/pages/performers/PerformerMerge.tsx
+++ frontend/src/pages/performers/PerformerMerge.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { flatMap } from "lodash-es";
