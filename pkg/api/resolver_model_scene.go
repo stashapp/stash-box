e239f4085cdcd08d232006f20fb96a08820569d5
peolic <66393006+peolic@users.noreply.github.com>
2022-02-14 01:52:35 +0200

    update backend

--- pkg/api/resolver_model_scene.go
+++ pkg/api/resolver_model_scene.go
@@ -34,8 +34,8 @@ func (r *sceneResolver) Code(ctx context.Context, obj *models.Scene) (*string, e
 	return resolveNullString(obj.Code), nil
 }
 
-func (r *sceneResolver) Date(ctx context.Context, obj *models.Scene) (*string, error) {
-	return resolveSQLiteDate(obj.Date)
+func (r *sceneResolver) Date(ctx context.Context, obj *models.Scene) (*models.FuzzyDate, error) {
+	return obj.ResolveDate(), nil
 }
 
 func (r *sceneResolver) Studio(ctx context.Context, obj *models.Scene) (*models.Studio, error) {
