5d1b2c1c7b6da610f51ec9aacc570d32835093ad
Infinite <infinitekittens@protonmail.com>
2022-05-15 22:04:10 +0000

    Update closed_at/updated_at values in edit updating migration

--- pkg/database/migrations/postgres/27_edit_closed_at.up.sql
+++ pkg/database/migrations/postgres/27_edit_closed_at.up.sql
@@ -3,3 +3,9 @@ ADD COLUMN "closed_at" timestamp;
 
 ALTER TABLE "edits"
 ALTER COLUMN "updated_at" DROP NOT NULL;
+
+UPDATE "edits"
+SET "closed_at" = "updated_at",
+"updated_at" = NULL
+WHERE "updated_at" > "created_at"
+AND "status" != 'PENDING';
