e0798fd3dbe62a90a42f17d128dc3985f81699c3
peolic <66393006+peolic@users.noreply.github.com>
2022-05-21 23:50:42 +0300

    StudioSelect parent studio (#395)

--- frontend/src/graphql/definitions/StudioEdit.ts
+++ frontend/src/graphql/definitions/StudioEdit.ts
@@ -189,10 +189,17 @@ export interface StudioEdit_studioEdit_target_Scene_images {
   height: number;
 }
 
+export interface StudioEdit_studioEdit_target_Scene_studio_parent {
+  __typename: "Studio";
+  id: string;
+  name: string;
+}
+
 export interface StudioEdit_studioEdit_target_Scene_studio {
   __typename: "Studio";
   id: string;
   name: string;
+  parent: StudioEdit_studioEdit_target_Scene_studio_parent | null;
 }
 
 export interface StudioEdit_studioEdit_target_Scene_performers_performer {
@@ -1317,10 +1324,17 @@ export interface StudioEdit_studioEdit_merge_sources_Scene_images {
   height: number;
 }
 
+export interface StudioEdit_studioEdit_merge_sources_Scene_studio_parent {
+  __typename: "Studio";
+  id: string;
+  name: string;
+}
+
 export interface StudioEdit_studioEdit_merge_sources_Scene_studio {
   __typename: "Studio";
   id: string;
   name: string;
+  parent: StudioEdit_studioEdit_merge_sources_Scene_studio_parent | null;
 }
 
 export interface StudioEdit_studioEdit_merge_sources_Scene_performers_performer {
