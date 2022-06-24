0917e6f47f21358758be91e5a2ed8b3548e86411
JackStack <100418085+JackDawson94@users.noreply.github.com>
2022-06-24 14:09:45 +0200

    now prettier

--- frontend/src/utils/createClient.ts
+++ frontend/src/utils/createClient.ts
@@ -20,13 +20,17 @@ const typePolicies: TypePolicies = {
 const isDevEnvironment = () => import.meta.env.DEV;
 
 export const getCredentialsSetting = () =>
-  isDevEnvironment() && !import.meta.env.VITE_SERVER_URL ? "include" : "same-origin";
+  isDevEnvironment() && !import.meta.env.VITE_SERVER_URL
+    ? "include"
+    : "same-origin";
 
 export const getPlatformURL = () => {
   let platformUrl = new URL(window.location.origin);
 
-  if (isDevEnvironment()){
-    platformUrl = new URL(import.meta.env.VITE_SERVER_URL ?? window.location.origin);
+  if (isDevEnvironment()) {
+    platformUrl = new URL(
+      import.meta.env.VITE_SERVER_URL ?? window.location.origin
+    );
     platformUrl.port = import.meta.env.VITE_SERVER_PORT ?? "9998";
   }
 
