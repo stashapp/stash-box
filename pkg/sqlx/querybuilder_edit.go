03692d7b4b10251cc1520a3e424ad45c72869128
Infinite <infinitekittens@protonmail.com>
2022-09-21 10:09:23 +0000

    Conditional querying for scene existence query

--- pkg/sqlx/querybuilder_edit.go
+++ pkg/sqlx/querybuilder_edit.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/gofrs/uuid"
 	"github.com/jmoiron/sqlx"
@@ -399,28 +400,40 @@ func (qb *editQueryBuilder) FindCompletedEdits(votingPeriod int, minimumVotingPe
 }
 
 func (qb *editQueryBuilder) FindPendingSceneCreation(input models.QueryExistingSceneInput) ([]*models.Edit, error) {
-	// jsonb_exists_any is the backing function for the ?: operator, but since sqlx foolishly has no way of escaping
-	// question marks in operators we have to use this undocumented function
+	if (input.StudioID == nil || input.Title == nil) && len(input.Fingerprints) == 0 {
+		return nil, nil
+	}
+
+	var clauses []string
+	arg := make(map[string]interface{})
+
+	if input.Title != nil && input.StudioID != nil {
+		arg["title"] = *input.Title
+		arg["studio"] = *input.StudioID
+		clauses = append(clauses, `
+			(data->'new_data'->>'title' = :title AND data->'new_data'->>'studio_id' = :studio)
+		`)
+	}
+	if len(input.Fingerprints) > 0 {
+		var hashes []string
+		for _, fp := range input.Fingerprints {
+			hashes = append(hashes, fp.Hash)
+		}
+		arg["hashes"] = pq.Array(hashes)
+
+		// jsonb_exists_any is the backing function for the ?: operator, but since sqlx foolishly has no way of escaping
+		// question marks in operators we have to use this undocumented function
+		clauses = append(clauses, `
+			(jsonb_exists_any(jsonb_path_query_array(data, '$.new_data.added_fingerprints[*].hash'), :hashes))
+		`)
+	}
+
 	query := `
 		SELECT edits.* FROM edits
 		WHERE status = 'PENDING'
 		AND target_type = 'SCENE'
-		AND ((
-			jsonb_exists_any(jsonb_path_query_array(data, '$.new_data.added_fingerprints[*].hash'), :hashes)
-		) OR (
-			data->'new_data'->>'title' = :title AND data->'new_data'->>'studio_id' = :studio
-		))
-	`
-
-	var hashes []string
-	for _, fp := range input.Fingerprints {
-		hashes = append(hashes, fp.Hash)
-	}
-	arg := map[string]interface{}{
-		"hashes": pq.Array(hashes),
-		"title":  input.Title,
-		"studio": input.StudioID,
-	}
+		AND
+	` + strings.Join(clauses, " OR ")
 
 	query, args, err := sqlx.Named(query, arg)
 	if err != nil {
