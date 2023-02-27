2c91d872a8fdd8e692f779663f329d4041ca0295
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2023-02-27 06:55:44 +0000

    Update react init

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,9 @@
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-
-render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+} else {
+  throw Error("Root not found")
+}
