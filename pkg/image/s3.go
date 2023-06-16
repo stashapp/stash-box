9212f33a0cefa46280467287ef69573218c9c9e1
WithoutPants <53250216+WithoutPants@users.noreply.github.com>
2023-06-16 13:28:14 +1000

    Allow configuration of s3 upload headers

--- pkg/image/s3.go
+++ pkg/image/s3.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 	"net/http"
 
 	"github.com/minio/minio-go/v7"
@@ -17,35 +18,37 @@ type S3Backend struct{}
 
 func (s *S3Backend) WriteFile(file *bytes.Reader, image *models.Image) error {
 	s3config := config.GetS3Config()
+	headers := s3config.UploadHeaders
+
 	minioClient, err := minio.New(s3config.Endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(s3config.AccessKey, s3config.Secret, ""),
 		Secure: true,
 	})
 	if err != nil {
-		return err
+		return fmt.Errorf("creating minio client: %w", err)
 	}
 
 	buf := new(bytes.Buffer)
 	if _, err = buf.ReadFrom(file); err != nil {
-		return err
+		return fmt.Errorf("reading from file: %w", err)
 	}
-	if err := uploadS3File(*minioClient, buf.Bytes(), s3config.Bucket, image.ID.String()); err != nil {
-		return err
+	if err := uploadS3File(*minioClient, buf.Bytes(), s3config.Bucket, image.ID.String(), headers); err != nil {
+		return fmt.Errorf("uploading to s3: %w", err)
 	}
 
 	if s3config.MaxDimension != 0 && (image.Width > s3config.MaxDimension || image.Height > s3config.MaxDimension) {
 		if _, err = file.Seek(0, 0); err != nil {
-			return err
+			return fmt.Errorf("seeking in file: %w", err)
 		}
 		resized, err := resizeImage(file, s3config.MaxDimension)
 		if err != nil {
-			return err
+			return fmt.Errorf("resizing image: %w", err)
 		}
 
 		hash := md5.Sum([]byte(image.ID.String() + "-resized"))
 		resizedID := hex.EncodeToString(hash[:])
-		if err := uploadS3File(*minioClient, resized, s3config.Bucket, resizedID); err != nil {
-			return err
+		if err := uploadS3File(*minioClient, resized, s3config.Bucket, resizedID, headers); err != nil {
+			return fmt.Errorf("uploading resized image to s3: %w", err)
 		}
 	}
 
@@ -79,7 +82,7 @@ func (s *S3Backend) DestroyFile(image *models.Image) error {
 	return nil
 }
 
-func uploadS3File(client minio.Client, file []byte, bucket string, id string) error {
+func uploadS3File(client minio.Client, file []byte, bucket string, id string, headers map[string]string) error {
 	ctx := context.TODO()
 
 	// SVG is not correctly detected so we set it manually if the file is xml
@@ -96,7 +99,8 @@ func uploadS3File(client minio.Client, file []byte, bucket string, id string) er
 		bytes.NewReader(file),
 		int64(len(file)),
 		minio.PutObjectOptions{
-			ContentType: contentType,
+			ContentType:  contentType,
+			UserMetadata: headers,
 		},
 	)
 
