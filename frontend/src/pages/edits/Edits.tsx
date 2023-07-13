256fcf9fdac74466b884cef1c9ebe85e8eabb9be
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2023-07-13 09:58:12 +0200

    Expand bot edit filter (#680)

--- frontend/src/pages/edits/Edits.tsx
+++ frontend/src/pages/edits/Edits.tsx
@@ -11,6 +11,7 @@ const EditsComponent: FC = () => (
     <EditList
       defaultVoteStatus={VoteStatusEnum.PENDING}
       defaultVoted={UserVotedFilterEnum.NOT_VOTED}
+      defaultBot="exclude"
     />
   </>
 );
