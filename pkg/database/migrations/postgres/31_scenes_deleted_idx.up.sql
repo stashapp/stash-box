4597cfa926e1b41fc15dba9ea1e65f685ee3fce1
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2023-07-20 15:35:57 +0000

    Additional indexes

--- pkg/database/migrations/postgres/31_scenes_deleted_idx.up.sql
+++ pkg/database/migrations/postgres/31_scenes_deleted_idx.up.sql
@@ -1,1 +1,3 @@
 CREATE INDEX "scenes_deleted_idx" ON "scenes"("deleted");
+CREATE INDEX "studio_favorites_idx" ON "studio_favorites"("studio_id", "user_id");
+CREATE INDEX "performer_favorites_idx" ON "performer_favorites"("performer_id", "user_id");
