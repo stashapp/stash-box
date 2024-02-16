ba8189baf12a4d8fc20e84749b7d13ca492a323d
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2024-02-16 09:11:02 +0000

    Additional index for fingerprint queries

--- pkg/database/migrations/postgres/31_scenes_deleted_idx.up.sql
+++ pkg/database/migrations/postgres/31_scenes_deleted_idx.up.sql
@@ -1,3 +1,4 @@
 CREATE INDEX "scenes_deleted_idx" ON "scenes"("deleted");
+CREATE INDEX "scenes_id_deleted_idx" ON "scenes"("id", "deleted");
 CREATE INDEX "studio_favorites_idx" ON "studio_favorites"("studio_id", "user_id");
 CREATE INDEX "performer_favorites_idx" ON "performer_favorites"("performer_id", "user_id");
