0a05b9c586569cba0a86591cf815930adcdfd7a1
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2024-02-28 23:01:16 +0100

    Drop unique index on pending invites and fix invite key list (#765)

--- pkg/sqlx/querybuilder_invite_key.go
+++ pkg/sqlx/querybuilder_invite_key.go
@@ -109,8 +109,13 @@ func (qb *inviteKeyQueryBuilder) Find(id uuid.UUID) (*models.InviteKey, error) {
 
 func (qb *inviteKeyQueryBuilder) FindActiveKeysForUser(userID uuid.UUID, expireTime time.Time) (models.InviteKeys, error) {
 	query := `SELECT i.* FROM ` + inviteKeyTable + ` i 
-	 LEFT JOIN ` + pendingActivationTable + ` a ON a.invite_key = i.id AND a.time > ?
-	 WHERE i.generated_by = ? AND a.id IS NULL`
+	 LEFT JOIN (
+	   SELECT invite_key, COUNT(*) as count
+		 FROM pending_activations
+		 WHERE time > ?
+		 GROUP BY invite_key
+	 ) a ON a.invite_key = i.id
+	 WHERE i.generated_by = ? AND (a.invite_key IS NULL OR i.uses IS NULL OR a.count < i.uses)`
 	var args []interface{}
 	args = append(args, expireTime)
 	args = append(args, userID)
