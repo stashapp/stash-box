82b955ca45b34824c5a56501bb6b179330f4a2a5
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2024-11-21 10:04:27 +0000

    Format

--- frontend/src/pages/sites/siteForm/SiteForm.tsx
+++ frontend/src/pages/sites/siteForm/SiteForm.tsx
@@ -39,7 +39,7 @@ const SiteForm: FC<SiteProps> = ({ site, callback }) => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm < SiteFormData > ({
+  } = useForm<SiteFormData>({
     resolver: yupResolver(schema),
   });
 
@@ -102,7 +102,8 @@ const SiteForm: FC<SiteProps> = ({ site, callback }) => {
           <br />
           Example:
           <br />
-          This regexp <code>(https?:\/\/(?:www\.)?(?:(.*)\.)?example\.com\/?[^?#]+)</code>
+          This regexp{" "}
+          <code>(https?:\/\/(?:www\.)?(?:(.*)\.)?example\.org\/?[^?#]+)</code>
           <br />
           will match this string{" "}
           <code>http://example.org/foo/bar?id=69#top</code>
