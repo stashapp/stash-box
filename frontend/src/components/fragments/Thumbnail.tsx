34b8684b56cbf667aa9e079017cc37363a81c0b2
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2024-12-29 21:57:15 +0000

    Fix display issues for scenes without images

--- frontend/src/components/fragments/Thumbnail.tsx
+++ frontend/src/components/fragments/Thumbnail.tsx
@@ -1,17 +1,24 @@
 import { FC } from "react";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import Icon from "./Icon";
 
 interface Props {
-  image: string;
+  image?: string;
   size?: number;
   alt?: string | null;
   className?: string;
 }
 
-export const Thumbnail: FC<Props> = ({ image, size, alt, className }) => (
-  <img
-    alt={alt ?? ""}
-    className={className}
-    src={image + (size ? `?size=${size}` : "")}
-    srcSet={size ? `${image}?size=${size * 2} ${size * 2}w` : ""}
-  />
-);
+export const Thumbnail: FC<Props> = ({ image, size, alt, className }) =>
+  image ? (
+    <img
+      alt={alt ?? ""}
+      className={className}
+      src={image + (size ? `?size=${size}` : "")}
+      srcSet={size ? `${image}?size=${size * 2} ${size * 2}w` : ""}
+    />
+  ) : (
+    <div className="Thumbnail-empty" style={{ aspectRatio: "16/9" }}>
+      <Icon icon={faXmark} />
+    </div>
+  );
