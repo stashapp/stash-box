288f845a31633aa5709e86f3cc4690c1784a0303
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2025-11-15 09:47:52 +0100

    Fix windows image resizing

--- internal/image/resize_windows.go
+++ internal/image/resize_windows.go
@@ -3,8 +3,14 @@
 package image
 
 import (
+	"bytes"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"io"
 
+	"github.com/disintegration/imaging"
+	"github.com/stashapp/stash-box/internal/config"
 	"github.com/stashapp/stash-box/internal/models"
 )
 
@@ -13,3 +19,36 @@ func Resize(reader io.Reader, max int, dbimage *models.Image, fileSize int64) ([
 }
 
 func InitResizer() {}
+
+func resizeImage(srcReader io.Reader, maxDimension int64) ([]byte, error) {
+	var resizedImage image.Image
+	srcImage, format, err := image.Decode(srcReader)
+	if err != nil {
+		return nil, err
+	}
+
+	// if height is longer then resize by height instead of width
+	if dim := srcImage.Bounds().Max; dim.Y > dim.X {
+		resizedImage = imaging.Resize(srcImage, 0, int(maxDimension), imaging.MitchellNetravali)
+	} else {
+		resizedImage = imaging.Resize(srcImage, int(maxDimension), 0, imaging.MitchellNetravali)
+	}
+
+	buf := new(bytes.Buffer)
+
+	if format == "png" {
+		err = png.Encode(buf, resizedImage)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		options := jpeg.Options{
+			Quality: config.GetImageJpegQuality(),
+		}
+		err = jpeg.Encode(buf, resizedImage, &options)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return buf.Bytes(), nil
+}
