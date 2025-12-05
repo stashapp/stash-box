f0841a0af5034f30bb877a51208c7f4cd3d495bf
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2025-12-05 13:22:54 +0100

    Refactor date string parsing

--- pkg/utils/date.go
+++ pkg/utils/date.go
@@ -5,47 +5,20 @@ import (
 	"time"
 )
 
-const railsTimeLayout = "2006-01-02 15:04:05 MST"
-
-// PadIncompleteDate pads incomplete date strings (YYYY or YYYY-MM) to complete dates (YYYY-MM-DD)
-func PadIncompleteDate(date string) string {
-	switch len(date) {
-	case 4:
-		return fmt.Sprintf("%s-01-01", date)
-	case 7:
-		return fmt.Sprintf("%s-01", date)
-	default:
-		return date
-	}
-}
-
 func ParseDateStringAsTime(dateString string) (time.Time, error) {
-	// https://stackoverflow.com/a/20234207 WTF?
-
-	t, e := time.Parse(time.RFC3339, dateString)
-	if e == nil {
-		return t, nil
-	}
-
-	t, e = time.Parse("2006-01-02", dateString)
-	if e == nil {
-		return t, nil
-	}
-
-	t, e = time.Parse("2006-01-02 15:04:05", dateString)
-	if e == nil {
-		return t, nil
-	}
-
-	t, e = time.Parse(railsTimeLayout, dateString)
-	if e == nil {
-		return t, nil
-	}
-
-	// Handle incomplete dates by padding with -01 or -01-01
-	paddedDateString := PadIncompleteDate(dateString)
-	if paddedDateString != dateString {
-		t, e = time.Parse("2006-01-02", paddedDateString)
+	switch len(dateString) {
+	case 4:
+		t, e := time.Parse("2006", dateString)
+		if e == nil {
+			return t, nil
+		}
+	case 7:
+		t, e := time.Parse("2006-01", dateString)
+		if e == nil {
+			return t, nil
+		}
+	default:
+		t, e := time.Parse("2006-01-02", dateString)
 		if e == nil {
 			return t, nil
 		}
