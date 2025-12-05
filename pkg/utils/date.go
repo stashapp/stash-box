492233307223d057e7922777d97a4837d279c97e
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2025-12-05 14:11:40 +0100

    Fix null age for partial birthdates (#1015)

--- pkg/utils/date.go
+++ pkg/utils/date.go
@@ -5,29 +5,23 @@ import (
 	"time"
 )
 
-const railsTimeLayout = "2006-01-02 15:04:05 MST"
-
 func ParseDateStringAsTime(dateString string) (time.Time, error) {
-	// https://stackoverflow.com/a/20234207 WTF?
-
-	t, e := time.Parse(time.RFC3339, dateString)
-	if e == nil {
-		return t, nil
-	}
-
-	t, e = time.Parse("2006-01-02", dateString)
-	if e == nil {
-		return t, nil
-	}
-
-	t, e = time.Parse("2006-01-02 15:04:05", dateString)
-	if e == nil {
-		return t, nil
-	}
-
-	t, e = time.Parse(railsTimeLayout, dateString)
-	if e == nil {
-		return t, nil
+	switch len(dateString) {
+	case 4:
+		t, e := time.Parse("2006", dateString)
+		if e == nil {
+			return t, nil
+		}
+	case 7:
+		t, e := time.Parse("2006-01", dateString)
+		if e == nil {
+			return t, nil
+		}
+	default:
+		t, e := time.Parse("2006-01-02", dateString)
+		if e == nil {
+			return t, nil
+		}
 	}
 
 	return time.Time{}, fmt.Errorf("ParseDateStringAsTime failed: dateString <%s>", dateString)
