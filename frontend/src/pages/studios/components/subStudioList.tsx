2a303ba9d5c75c44acb436ce446fd52aaf074b4a
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2026-03-22 19:05:45 +0100

    Split component

--- frontend/src/pages/studios/components/subStudioList.tsx
+++ frontend/src/pages/studios/components/subStudioList.tsx
@@ -6,61 +6,13 @@ import { debounce } from "lodash-es";
 import { useSubStudios, SortDirectionEnum, StudioSortEnum } from "src/graphql";
 import { usePagination } from "src/hooks";
 import { List } from "src/components/list";
-import { LoadingIndicator } from "src/components/fragments";
 import { studioHref } from "src/utils";
 
 const PER_PAGE = 25;
-const PREVIEW_COUNT = 25;
 
 interface Props {
   id: string;
 }
-
-interface PreviewProps {
-  id: string;
-  onViewAll: () => void;
-}
-
-export const SubStudioPreview: FC<PreviewProps> = ({ id, onViewAll }) => {
-  const { data, loading } = useSubStudios({
-    id,
-    input: {
-      page: 1,
-      per_page: PREVIEW_COUNT,
-      sort: StudioSortEnum.NAME,
-      direction: SortDirectionEnum.ASC,
-    },
-  });
-
-  const studios = data?.findStudio?.sub_studios.studios;
-  const count = data?.findStudio?.sub_studios.count ?? 0;
-  const hasMore = count > PREVIEW_COUNT;
-
-  if (loading) return <LoadingIndicator message="Loading sub-studios..." />;
-
-  return (
-    <div className="sub-studio-list">
-      <ul>
-        {studios?.map((s) => (
-          <li key={s.id}>
-            <Link to={studioHref(s)}>{s.name}</Link>
-          </li>
-        ))}
-        {hasMore && (
-          <li key="view-all" style={{ listStyle: "none", marginLeft: "-1rem" }}>
-            <button
-              type="button"
-              className="btn btn-link p-0"
-              onClick={onViewAll}
-            >
-              View all {count} sub-studios
-            </button>
-          </li>
-        )}
-      </ul>
-    </div>
-  );
-};
 
 export const SubStudioList: FC<Props> = ({ id }) => {
   const [filter, setFilter] = useState("");
