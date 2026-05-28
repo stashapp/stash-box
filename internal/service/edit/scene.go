513115dff09f09373e6440bcbe5ef3767dc4c83b
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2026-05-28 16:03:14 +0200

    Remove fingerprints/images from deleted scenes

--- internal/service/edit/scene.go
+++ internal/service/edit/scene.go
@@ -421,7 +421,15 @@ func (m *SceneEditProcessor) applyDestroy(scene *models.Scene) error {
 		return err
 	}
 
-	return err
+	if err = m.queries.DeleteSceneImages(m.context, scene.ID); err != nil {
+		return err
+	}
+
+	if err = m.queries.DeleteSceneFingerprintsByScene(m.context, scene.ID); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (m *SceneEditProcessor) applyMerge(scene *models.Scene, data *models.SceneEditData) error {
