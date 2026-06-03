6c5e1606003ab874c0306483e86f25e3602f24f5
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2026-06-03 15:16:42 +0200

    Generated files

--- internal/queries/models.go
+++ internal/queries/models.go
@@ -242,7 +242,7 @@ type PerformerPiercing struct {
 	Description *string   `db:"description" json:"description"`
 }
 
-type PerformerPopularityAlltime struct {
+type PerformerPopularity struct {
 	PerformerID uuid.UUID `db:"performer_id" json:"performer_id"`
 	UserCount   int       `db:"user_count" json:"user_count"`
 }
@@ -312,13 +312,9 @@ type ScenePerformer struct {
 	PerformerID uuid.UUID `db:"performer_id" json:"performer_id"`
 }
 
-type ScenePopularityAlltime struct {
-	SceneID   uuid.UUID `db:"scene_id" json:"scene_id"`
-	UserCount int       `db:"user_count" json:"user_count"`
-}
-
-type ScenePopularityTrending struct {
+type ScenePopularity struct {
 	SceneID       uuid.UUID `db:"scene_id" json:"scene_id"`
+	UserCount     int       `db:"user_count" json:"user_count"`
 	TrendingCount int       `db:"trending_count" json:"trending_count"`
 }
 
