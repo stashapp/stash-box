d2e23f3e0467487a78d38959d471c9ce6a1fa289
InfiniteStash <117855276+InfiniteStash@users.noreply.github.com>
2026-06-07 10:42:07 +0200

    Nulls last

--- internal/database/migrations/postgres/69_scene_deleted_sort_duration_index.up.sql
+++ internal/database/migrations/postgres/69_scene_deleted_sort_duration_index.up.sql
@@ -1,1 +1,1 @@
-CREATE INDEX scenes_deleted_duration_id_idx ON scenes (duration DESC, id DESC) WHERE deleted = false;
+CREATE INDEX scenes_deleted_duration_id_idx ON scenes (duration DESC NULLS LAST, id DESC) WHERE deleted = false;
