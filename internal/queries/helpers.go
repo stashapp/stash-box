efd0d5acaedd4ef80dd031318677084ad95da6d2
Slick Daddy <129640104+slick-daddy@users.noreply.github.com>
2026-09-07 00:23:15 +0300

    fix nr12: batch query param pre-alloc

--- internal/queries/helpers.go
+++ internal/queries/helpers.go
@@ -1,5 +1,11 @@
 package queries
 
+func QueryIDs(ids []string) []string {
+	res := make([]string, 0, len(ids))
+	res = append(res, ids...)
+	return res
+}
+
 // DB returns the underlying DBTX interface from Queries
 func (q *Queries) DB() DBTX {
 	return q.db
